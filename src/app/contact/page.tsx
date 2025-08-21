44bab08eadae3357fe50dc766576ec77d2d7fc7b
DrZiMo <zuhaybhamar@gmail.com>
2025-08-21 19:40:10 +0300

    refactor: remove unnecessary whitespace in Contact component for cleaner code

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -12,9 +12,7 @@ const Contact = () => {
         title='Get in Touch'
         subTitle="Have questions about our platform? Want to partner with us? Or just want to say hello? We'd love to hear from you. Let's start a conversation."
       />
-
       <ContactOptions />
-
       <div className='mt-32 grid grid-cols-2 gap-10'>
         <SendUsMessage />
         <div className='flex flex-col gap-5'>
