d0bd4415d1ada6fdda6f412316005eb6b8bde3e8
DrZiMo <zuhaybhamar@gmail.com>
2025-08-29 00:29:29 +0300

    added the user profile picture in the header

--- src/components/UserPart.tsx
+++ src/components/UserPart.tsx
@@ -5,26 +5,37 @@ import { ModeToggle } from './ModeToggle'
 import { Button } from './ui/button'
 import Link from 'next/link'
 import { MobileSidebar } from './MobileSidebar'
+import { useSelector } from 'react-redux'
+import { RootState } from '@/store'
+import UserProfilePicture from './UserProfilePicture'
 
 const UserPart = () => {
+  const isLoggedIn = useSelector((state: RootState) => state.auth.isLoggedIn)
+  const user = useSelector((state: RootState) => state.auth.user)
   return (
-    <div className='flex gap-8'>
-      <div className='flex gap-2'>
+    <div className='flex items-center gap-8'>
+      <div className='flex items-center gap-2'>
         <ModeToggle />
         <div className='md:hidden block'>
           <MobileSidebar />
         </div>
       </div>
 
-      {/* if there is no logged in user */}
-      <div className='buttons md:flex gap-3 hidden'>
-        <Link href={'/auth/login'}>
-          <Button variant={'outline'}>Login</Button>
-        </Link>
-        <Link href={'/auth/signup'}>
-          <Button>Signup</Button>
-        </Link>
-      </div>
+      {isLoggedIn ? (
+        <UserProfilePicture user={user!} />
+      ) : (
+        <>
+          {/* if there is no logged in user */}
+          <div className='buttons md:flex gap-3 hidden'>
+            <Link href={'/auth/login'}>
+              <Button variant={'outline'}>Login</Button>
+            </Link>
+            <Link href={'/auth/signup'}>
+              <Button>Signup</Button>
+            </Link>
+          </div>
+        </>
+      )}
     </div>
   )
 }
---
+++ src/components/UserProfilePicture.tsx
@@ -0,0 +1,91 @@
+import React from 'react'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu'
+import { Button } from './ui/button'
+import { UserProps } from '@/app/types/users.types'
+import { User } from 'lucide-react'
+
+const UserProfilePicture = ({ user }: { user: UserProps }) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <div className='w-10 h-10 rounded-full cursor-pointer'>
+          {user.profile_pic ? (
+            <img
+              src={user.profile_pic}
+              alt={user.first_name}
+              className='w-full h-full object-cover rounded-full'
+            />
+          ) : (
+            <div className='bg-primary w-full h-full flex items-center justify-center rounded-full'>
+              <User />
+            </div>
+          )}
+        </div>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className='w-56' align='start'>
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            Profile
+            <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            Billing
+            <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            Settings
+            <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            Keyboard shortcuts
+            <DropdownMenuShortcut>⌘K</DropdownMenuShortcut>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuSub>
+            <DropdownMenuSubTrigger>Invite users</DropdownMenuSubTrigger>
+            <DropdownMenuPortal>
+              <DropdownMenuSubContent>
+                <DropdownMenuItem>Email</DropdownMenuItem>
+                <DropdownMenuItem>Message</DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>More...</DropdownMenuItem>
+              </DropdownMenuSubContent>
+            </DropdownMenuPortal>
+          </DropdownMenuSub>
+          <DropdownMenuItem>
+            New Team
+            <DropdownMenuShortcut>⌘+T</DropdownMenuShortcut>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>GitHub</DropdownMenuItem>
+        <DropdownMenuItem>Support</DropdownMenuItem>
+        <DropdownMenuItem disabled>API</DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          Log out
+          <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
+
+export default UserProfilePicture
