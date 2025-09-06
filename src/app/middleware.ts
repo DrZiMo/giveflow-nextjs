683a6898d23a6df0a21abfc97f9f846abe45b0f5
DrZiMo <zuhaybhamar@gmail.com>
2025-09-06 13:15:30 +0300

    added the middlware to protect the admin

---
+++ src/app/middleware.ts
@@ -0,0 +1,19 @@
+import { RootState } from '@/store'
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import { useSelector } from 'react-redux'
+import { ROLE } from './types/users.types'
+
+export function Middleware(request: NextRequest) {
+  const { user, isLoggedIn } = useSelector((state: RootState) => state.auth)
+
+  if (!user || !isLoggedIn || user.role !== ROLE.ADMIN) {
+    return NextResponse.redirect(new URL('/', request.url))
+  }
+
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: '/dashboard/:path*',
+}
