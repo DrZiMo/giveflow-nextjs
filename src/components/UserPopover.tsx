cc224625562be562dac9e43050f485130faa811b
DrZiMo <zuhaybhamar@gmail.com>
2025-09-08 22:33:06 +0300

    edited the user popover

--- src/components/UserPopover.tsx
+++ src/components/UserPopover.tsx
@@ -7,11 +7,16 @@ import {
   DialogTitle,
   DialogTrigger,
 } from '@/components/ui/dialog'
-import { Eye, User } from 'lucide-react'
+import { Ban, Eye, Mail, Pencil, Phone, User } from 'lucide-react'
 import Loading from '@/app/loading'
 import { useSingleUser } from '@/lib/hook/useUser'
 import { Badge } from './ui/badge'
 import { ROLE } from '@/app/types/users.types'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import dayjs from 'dayjs'
+import { Button } from './ui/button'
+
+dayjs.extend(relativeTime)
 
 const UserPopover = ({
   userId,
@@ -42,33 +47,76 @@ const UserPopover = ({
         {isLoading ? (
           <Loading />
         ) : (
-          <div className='flex items-center gap-5'>
-            <div className='w-20 h-20 rounded-full flex-shrink-0'>
-              {user?.profile_pic ? (
-                <img
-                  src={user.profile_pic}
-                  alt={user.first_name}
-                  className='w-full h-full object-cover rounded-full'
-                />
-              ) : (
-                <div className='w-full h-full bg-primary rounded-full flex justify-center items-center'>
-                  <User className='w-5 h-5' />
-                </div>
-              )}
-            </div>
-            <div>
-              <div className='flex items-center gap-2'>
-                <h1 className='font-medium text-card-foreground'>
-                  {user?.first_name} {user?.last_name}
-                </h1>
-                {user?.role === ROLE.ADMIN ? (
-                  <Badge variant={'success'}>{ROLE.ADMIN}</Badge>
-                ) : null}
+          <div>
+            <div className='flex items-center gap-5'>
+              <div className='w-20 h-20 rounded-full flex-shrink-0'>
+                {user?.profile_pic ? (
+                  <img
+                    src={user.profile_pic}
+                    alt={user.first_name}
+                    className='w-full h-full object-cover rounded-full'
+                  />
+                ) : (
+                  <div className='w-full h-full bg-primary rounded-full flex justify-center items-center'>
+                    <User className='w-5 h-5' />
+                  </div>
+                )}
               </div>
-              <p className='text-muted-foreground text-sm'>{user?.email}</p>
-              <Badge variant={user?.is_deleted ? 'destructive' : 'success'}>
-                {user?.is_deleted ? 'Suspended' : 'Active'}
-              </Badge>
+              <div>
+                <div className='flex items-center gap-2'>
+                  <h1 className='font-medium text-card-foreground'>
+                    {user?.first_name} {user?.last_name}
+                  </h1>
+                  {user?.role === ROLE.ADMIN ? (
+                    <Badge variant={'success'}>{ROLE.ADMIN}</Badge>
+                  ) : null}
+                </div>
+                <p className='text-muted-foreground text-sm'>{user?.email}</p>
+                <Badge variant={user?.is_deleted ? 'destructive' : 'success'}>
+                  {user?.is_deleted ? 'Suspended' : 'Active'}
+                </Badge>
+              </div>
+            </div>
+
+            <div className='grid grid-cols-1 md:grid-cols-2 mt-8'>
+              <div>
+                <h3 className='text-muted-foreground font-medium'>
+                  Contact Information
+                </h3>
+                <div className='flex gap-2 items-center text-sm my-2'>
+                  <Mail className='text-muted-foreground w-4 h-4' />
+                  <p>{user?.email}</p>
+                </div>
+                <div className='flex gap-2 items-center text-sm'>
+                  <Phone className='text-muted-foreground w-4 h-4' />
+                  <p>
+                    {user?.phone_number ? user.phone_number : 'No phone number'}
+                  </p>
+                </div>
+              </div>
+              <div>
+                <h3 className='text-muted-foreground font-medium'>
+                  Account Details
+                </h3>
+                <div className='flex gap-1 items-center my-2'>
+                  <p className='text-muted-foreground'>User Role:</p>
+                  <p>{user?.role}</p>
+                </div>
+                <div className='flex gap-1 items-center'>
+                  <p className='text-muted-foreground'>Joined:</p>
+                  <p>{dayjs(user?.created_at).fromNow()}</p>
+                </div>
+              </div>
+            </div>
+
+            <div className='flex justify-end gap-2 mt-6 items-center'>
+              <Button variant={'outline'}>Cancel</Button>
+              <Button variant={'default'}>
+                <Pencil /> Edit
+              </Button>
+              <Button variant={'destructive'}>
+                <Ban /> Suspend
+              </Button>
             </div>
           </div>
         )}
